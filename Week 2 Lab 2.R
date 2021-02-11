3040772919ed01db0060123527257bf79d19f131
Pritesh Maheshwari <priteshgames@gmail.com>
2021-02-11 09:53:10 -0500

    Feb 11, 2021 Class

---
+++ Week 2 Lab 2.R
@@ -0,0 +1,27 @@
+#Lab on Feb 11, 2021
+#Classwork
+
+temperature <- 85
+class(temperature)
+RPI = "Rensselaer Polytechnic Institute"
+RPI
+
+
+num_vec <- c(1,2,5,6,8)
+class(num_vec)
+chr_vec = c('R', 'P', 'I')
+chr_vec
+bool_vec =c(T,T,F,FALSE)
+bool_vec
+
+mix_vec= c('CHaracter',T ,1232,44)
+mix_vec
+class(mix_vec)
+
+num_m_vec= c(T ,1232,44)
+num_m_vec
+class(num_m_vec)
+
+temperature <- c(70,71,72,70.3,74,75,76)
+names(temperature) <- c("Mon", "Tue", "Wed","Thrus", "Fri", "Sat", "Sun")
+temperature
